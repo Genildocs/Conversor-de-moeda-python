10306aeffcc66e958d25a783d64fe443b48b53d3
Genildo <genildocs@gmail.com>
2022-06-15 05:46:45 -0300

    primeiro commit

---
+++ conversaodemoedas.py
@@ -0,0 +1,7 @@
+real=float(input('Qual valor você quer converter em dolar/euro? R$ '))
+dolar=real//5.12
+euro=real//5.37
+print('Valores convertidos: ')
+print(f'O valor de R$ {real:.1f} em conversão em dólar é: US$: {dolar:.1f} e Euros: {euro:.1f}')
+
+
